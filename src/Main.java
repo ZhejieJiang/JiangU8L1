3cc081397a36d671ad47da8d13e2d23e9622d6d4
Zhejie Jiang <zhejiej@nycstudents.net>
2023-03-06 09:29:34 -0500

    working

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Main {
+public class Main
+{
+    public static void main(String[]args)
+    {
+        String[][] seatingChart = {
+
+                {"Abby", "Don", "George", "Kim"}
+                {"Brian", "Eleanor", "Harry", "Lenny"}
+                {"Cathy","Fred", "Jill", "Matt"}
+        };
+
+        seatingChart[1][2] = "Paul";
+
+        String temp = seatingChart[0][0];
+        seatingChart[0][0] = seatingChart[2][3];
+        seatingChart[2][3] = temp;
+
+        String[] tempe = seatingChart[0];
+        seatingChart[0] = seatingChart[1];
+        seatingChart[1] = tempe;
+    }
 }
