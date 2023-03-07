f0e056a6b323c2b4b80cab9f29d884688d11f293
Zhejie Jiang <zhejiej@nycstudents.net>
2023-03-07 08:28:39 -0500

    finish

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,11 @@
+import java.util.Arrays;
+
 public class Main
 {
     public static void main(String[]args)
     {
-        String[][] seatingChart = {
-
-                {"Abby", "Don", "George", "Kim"}
-                {"Brian", "Eleanor", "Harry", "Lenny"}
-                {"Cathy","Fred", "Jill", "Matt"}
-        };
+        /*
+        String[][] seatingChart = {{"Abby", "Don", "George", "Kim"},{"Brian", "Eleanor", "Harry", "Lenny"},{"Cathy","Fred", "Jill", "Matt"}};
 
         seatingChart[1][2] = "Paul";
 
@@ -18,5 +16,37 @@ public class Main
         String[] tempe = seatingChart[0];
         seatingChart[0] = seatingChart[1];
         seatingChart[1] = tempe;
+
+        for(String[] seats: seatingChart)
+        {
+            System.out.println(Arrays.toString(seats));
+        }
+        */
+
+      int[][] arr1 = new int[2][3];
+      arr1[0][0] = 1;
+      arr1[0][1] = 2;
+      arr1[0][2] = 3;
+      arr1[1][0] = 4;
+      arr1[1][1] = 5;
+      arr1[1][2] = 6;
+
+        for(int[] nums: arr1)
+        {
+            System.out.println(Arrays.toString(nums));
+        }
+
+        int[][] arr2 = new int[3][2];
+        arr2[0][0] = 1;
+        arr2[0][1] = 4;
+        arr2[1][0] = 2;
+        arr2[1][1] = 5;
+        arr2[2][0] = 3;
+        arr2[2][1] = 6;
+
+        for(int[] nums: arr2)
+        {
+            System.out.println(Arrays.toString(nums));
+        }
     }
 }
